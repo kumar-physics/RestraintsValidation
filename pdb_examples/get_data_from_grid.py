cb9ae7b7c5c7be981c69ab37553e03c06968d096
Kumaran Baskaran <kbaskaran2@wisc.edu>
2019-04-25 10:30:20 -0500

    cif files added

--- pdb_examples/get_data_from_grid.py
+++ pdb_examples/get_data_from_grid.py
@@ -10,13 +10,21 @@ def get_data_from_gird(flist):
     pdb_ids = []
     for r in f.split("\n"):
         pdb_ids+=r.split(" ")
-    for pdb in pdb_ids:
-        try:
-            url = 'http://www.bmrb.wisc.edu/ftp/pub/bmrb/nmr_pdb_integrated_data/coordinates_restraints_chemshifts' \
-                    '/all/nmr-star/{}/{}_linked.str'.format(pdb.lower(),pdb.lower())
-            wget.download(url)
-        except urllib.error.HTTPError:
-            print (url)
+    pl = [i.lower() for i in pdb_ids]
+    f = open('idlist.txt','w')
+    f.write("\n".join(pl))
+    f.close()
+
+    # for pdb in pdb_ids:
+    #     try:
+    #         url = 'http://www.bmrb.wisc.edu/ftp/pub/bmrb/nmr_pdb_integrated_data/coordinates_restraints_chemshifts' \
+    #                 '/all/nmr-star/{}/{}_linked.str'.format(pdb.lower(),pdb.lower())
+    #         wget.download(url)
+    #         pdb_url = 'https://files.rcsb.org/download/{}.cif'.format(pdb)
+    #         wget.download(pdb_url,'{}.cif'.format(pdb.lower()))
+    #
+    #     except urllib.error.HTTPError:
+    #         print (pdb_url)
 
 if __name__ == "__main__":
     get_data_from_gird('list_from_pdb.txt')
